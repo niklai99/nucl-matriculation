9ccd210fad101f4c98c0258f5501d87be3776902
Nik <62773963+niklai99@users.noreply.github.com>
2021-03-18 00:08:17 +0100

    Updated text

--- fit.py
+++ fit.py
@@ -12,13 +12,14 @@ import re
 massP = 938.27208816 # Mev/c^2
 massN = 939.56542052 # Mev/c^2
 toMev = 931.493614838934 # ua -> Mev
+Ac = 0.714 # MeV
 
 def fetch_data(A,Z):
 
     # how many points [left]
-    nL = 3
+    nL = 2
     # how many points [right]
-    nR = 3
+    nR = 2
 
     Y=[]
     Y_e=[]
@@ -84,17 +85,19 @@ def make_fit(X, Y, Y_e, A_start):
 
     # fit
     par, cov=curve_fit(
-                        lambda Z,AV,AS,AC,AA: SEMF(Z,A_start,AV,AS,AC,AA),
+                        #lambda Z,AV,AS,AC,AA: SEMF(Z,A_start,AV,AS,AC,AA),
+                        lambda Z,AV,AS,AA: SEMF(Z,A_start,AV,AS,Ac,AA),
                         xdata = X, ydata = Y,
                         sigma=Y_e, absolute_sigma=True,
                         #p0= (15.6,17.23, 0.7, 23.28),
-                        bounds=([14,16,0,17],[17,19,2,25]),
+                        #bounds=([14,16,0,17],[17,19,2,25]),
+                        bounds=([14,16,10],[17,19,35])
                   ) 
 
     residuals = []
     chi2 = 0
     for i in range(len(X)):
-        residuals.append(Y[i] - SEMF(X[i], A_start, *par))
+        residuals.append(Y[i] - SEMF(X[i], A_start, par[0], par[1], Ac, par[2]))
         chi2 += (residuals[i] / Y_e[i])**2
 
     # get parameters error from cov matrix
@@ -124,10 +127,11 @@ def make_plot(X, Y, Y_e, A_start, par, par_e, chi2):
 
     # text template for parameters
     textA  = 'A  = ' + format(A_start, '1.0f')
-    textAV = 'AV = ' + format(par[0], '1.1f') + ' +/- ' + format(par_e[0],'1.1f')
-    textAS = 'AS = ' + format(par[1], '1.1f') + ' +/- ' + format(par_e[1],'1.1f')
-    textAC = 'AC = ' + format(par[2], '1.4f') + ' +/- ' + format(par_e[2],'1.4f')
-    textAA = 'AA = ' + format(par[3], '1.3f') + ' +/- ' + format(par_e[3],'1.3f')
+    textAV = '$A_V$ = ' + format(par[0], '1.1f') + ' $\pm$ ' + format(par_e[0],'1.1f') + ' MeV'
+    textAS = '$A_S$ = ' + format(par[1], '1.1f') + ' $\pm$ ' + format(par_e[1],'1.1f') + ' MeV'
+    #textAC = 'AC = ' + format(par[2], '1.4f') + ' $\pm$ ' + format(par_e[2],'1.4f') + ' MeV'
+    textAC = '$A_C$ = ' + format(Ac, '1.3f') + ' MeV' + ' fixed'
+    textAA = '$A_A$ = ' + format(par[2], '1.3f') + ' $\pm$ ' + format(par_e[2],'1.3f') + ' MeV'
     chisq  = '$\chi^{2}$ / ndf = ' + format(chi2, '1.0f') + ' / ' + format(len(X) - len(par), '1.0f')
 
     # plot data
@@ -135,7 +139,7 @@ def make_plot(X, Y, Y_e, A_start, par, par_e, chi2):
                 elinewidth=1, color = '#000000', linewidth=0, capsize=2, label = 'Data')
 
     # plot fit
-    ax.plot(np.arange(XMIN, XMAX, 0.01), SEMF(np.arange(XMIN, XMAX, 0.01), A_start, *par), 
+    ax.plot(np.arange(XMIN, XMAX, 0.01), SEMF(np.arange(XMIN, XMAX, 0.01), A_start, par[0], par[1], Ac, par[2]), 
             color = '#FF4B00', linewidth = 2, linestyle = '-', label = 'Fit')
 
     # display texts
@@ -162,8 +166,8 @@ def main():
     #print(nuc(44,101))
     #print(weight(44,101))
 
-    Z_start = 40 # Z di partenza
-    A_start = 91 # A di partenza
+    Z_start = 37 # Z di partenza
+    A_start = 85 # A di partenza
 
     # get data
     X, Y, Y_e = fetch_data(A_start,Z_start)
